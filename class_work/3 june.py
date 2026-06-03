ceea1a22acc8f57f310b75db9b1dd42f857ecb52
Ayush <ayush09042006@gmail.com>
2026-06-03 15:33:53 +0530

    classword1

--- class_work/3 june.py
+++ class_work/3 june.py
@@ -1,51 +1,15 @@
-# import math as m1
+import math as m1
 
 
-# side1= int(input("enter the side1 in cm=  "))
-# side2= int(input("enter the side2 in cm=  "))
-# side3= int(input("enter the side3 in cm=  "))
+side1= int(input("enter the side1 in cm=  "))
+side2= int(input("enter the side2 in cm=  "))
+side3= int(input("enter the side3 in cm=  "))
 
-# #parameter of traingle
-# parameter= side1 + side2 + side3
-# s=parameter/2
-# #area of triangle
-# area=m1.sqrt(s*(s-side1)*(s-side2)*(s-side3))
-# #display the result
-# print("area of given triangle",area)
-# print("parameter of triangle",parameter)
-
-
-
-
-
-
-
-
-
-
-
-''' programe which convert time in seconds into hours and minutes'''
-t=int(input("enter time in seconds="))
-if(t<0):
-    exit("Time cannot be negative.....Exited")
-
-print("................................")
-hours=0
-minutes=0
-
-'''convert into hours'''
-if(t>=3600):
-    hours=t//3600
-    remaningSeconds=t%3600
-#................................
-'''converts into  minutes'''
-if(t>=60):
-    minutes=t//60
-    seconds=t%60
-#................................
-'''display the results'''
-    
-    
-print("hours =",hours)
-print("minutes =",minutes)
-print("seconds =",t)
+#parameter of traingle
+parameter= side1 + side2 + side3
+s=parameter/2
+#area of triangle
+area=m1.sqrt(s*(s-side1)*(s-side2)*(s-side3))
+#display the result
+print("area of given triangle",area)
+print("parameter of triangle",parameter)

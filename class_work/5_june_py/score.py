5168b8b4fe137e8248b129c9e1e5044dd16a88fc
Ayush <ayush09042006@gmail.com>
2026-06-05 11:24:57 +0530

    score

---
+++ class_work/5_june_py/score.py
@@ -0,0 +1,19 @@
+a = int(input("Enter the number of players >>> "))
+
+total = 0
+#TO CHECK THE USE DO NOT TAKE NEGATIVE NUMEBR:
+if a <= 0:
+    print("Player count cannot be negative or zero")
+    a = int(input("RE-Enter the number of players >>> "))
+#ENTERY OF SCORE 
+for i in range(1, a + 1):
+    score = int(input(f"Score of player {i}: "))
+    # TO CHECK WHEATHER SCORE SHOULD NOT BE NEGATIVE:
+    while score < 0:
+        print("Invalid score!")
+        score = int(input(f"Enter the score again for player {i}: "))
+
+    total += score
+
+print("Total score of all players =", total)
+ 

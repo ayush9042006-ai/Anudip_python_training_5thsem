344428956bea619aed8856cc682b2e568806d4d2
Ayush <ayush09042006@gmail.com>
2026-06-05 16:04:26 +0530

    ...

--- class_work/5_june_py/busSEAT.py
+++ class_work/5_june_py/busSEAT.py
@@ -1,7 +1,9 @@
 seat=[1,0,1,1,0,0,1,1,1,0]
 count_book=0
 count_A=0
+avialable=[]
 '''.............................................................'''
+#count the booked seat and avialable seat
 for i in seat:
     if i==1:
         count_book+=1
@@ -9,7 +11,19 @@ for i in seat:
         count_A+=1
 
 
-
 print(f"BOOKED SEAT......{count_book}")
 print(f"avialable seat......{count_A}")
 
+
+# Find first available seat..........................................
+for i in range(len(seat)):
+    if seat[i] == 0:
+        print(f"First available seat = {i+1}")
+        break
+# Find all available seat numbers
+print("Available seat numbers:")
+
+for i in range(len(seat)):
+    if seat[i] == 0:
+        avialable.append(i+1)
+print(avialable)
